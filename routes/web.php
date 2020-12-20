ac198e7bf67d38003a851e3826bdb68c1cb6214c
haithem <haithem.soussi@gmail.com>
2020-12-20 23:43:36 +0100

    Ajouter une command artisan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Mail\ContactMessageCreated;
-
 Route::get('/', 'PagesController@home')->name('home');
 
 Route::get('/about', 'PagesController@about')->name('about');
@@ -9,7 +7,3 @@ Route::get('/about', 'PagesController@about')->name('about');
 Route::get('/contact', 'ContactsController@create')->name('contact.create');
 
 Route::post('/contact', 'ContactsController@store')->name('contact.store');
-
-Route::get('/test-email', function () {
-    return new ContactMessageCreated('Haithem Soussi', 'haithem.soussi@gmail.com', 'je suis la');
-});
