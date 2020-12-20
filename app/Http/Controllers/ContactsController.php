551f1bec05e38d195c1b327503961dced598136a
haithem <haithem.soussi@gmail.com>
2020-12-20 23:47:04 +0100

    Modification du controleur contact

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -5,23 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ContactRequest;
 use App\Mail\ContactMessageCreated;
 use App\Models\Message;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use MercurySeries\Flashy\Flashy;
 
-
 class ContactsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -44,50 +32,5 @@ class ContactsController extends Controller
         Mail::to(config('laracarte.admin_support_email'))->send(new ContactMessageCreated($message));
         Flashy::message('Nous vous répondrons dans les plus brefs délais!');
         return Redirect()->route('home');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
