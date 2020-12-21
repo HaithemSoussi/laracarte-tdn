aafbafd94dfa427ad79d5f47b27c99cacdf3b30f
haithem <haithem.soussi@gmail.com>
2020-12-21 17:48:42 +0100

    home page blade

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,9 +11,11 @@
                     <div class="card">
                         <div class="card-header">Dashboard</div>
                         <div class="card-body">
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
                             You are logged in!
                         </div>
                     </div>
