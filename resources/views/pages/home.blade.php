2921f8b903c96fa2f2befbed14dc73ae4de6852c
haithem <haithem.soussi@gmail.com>
2020-12-21 13:13:05 +0100

    Add Auth

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.default',['title' => 'Home'])
 
 @section('content')
-    <h1 class="text-center">Map here!!!!!!</h1>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @guest
+                    <h1 class="text-center">Map here!!!!!!</h1>
+                @else
+                    <div class="card">
+                        <div class="card-header">Dashboard</div>
+                        <div class="card-body">
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                            You are logged in!
+                        </div>
+                    </div>
+                @endguest
+            </div>
+        </div>
+    </div>
 @stop
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@ Route::view('/', 'pages.home')->name('home');
 Route::view('/about', 'pages.about')->name('about');
 Route::get('/contact', 'ContactsController@create')->name('contact_path');
 Route::post('/contact', 'ContactsController@store')->name('contact_path');
+Auth::routes();
